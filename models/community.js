1a0fdfe317612916fa1d3461e87de4770188a22f
thakurdinker <dinker.thakur55@gmail.com>
2024-07-02 20:22:52 +0400

    added community page api

--- models/community.js
+++ models/community.js
@@ -6,17 +6,20 @@ const communitySchema = new mongoose.Schema(
     slug: { type: String, required: true, unique: true }, // SEO-friendly URL
     description: { type: String }, // Detailed description
     location: {
+      _id: false,
       address: { type: String },
       city: { type: String },
       state: { type: String },
       country: { type: String },
       coordinates: {
+        _id: false,
         lat: { type: Number },
         lng: { type: Number },
       },
     },
     amenities: [
       {
+        _id: false,
         name: { type: String },
         icon_url: { type: String },
         description: { type: String },
@@ -24,26 +27,31 @@ const communitySchema = new mongoose.Schema(
     ],
     images: [
       {
+        _id: false,
         url: { type: String },
         description: { type: String },
       },
     ],
     faqs: [
       {
+        _id: false,
         question: { type: String },
         answer: { type: String },
       },
     ],
     seo: {
+      _id: false,
       meta_title: { type: String },
       meta_description: { type: String },
       keywords: [{ type: String }],
     },
     schema_org: {
+      _id: false,
       type: { type: String, default: "Place" },
       properties: { type: Object },
     },
     open_graph: {
+      _id: false,
       title: { type: String },
       description: { type: String },
       image: { type: String },

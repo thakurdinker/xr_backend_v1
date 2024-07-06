273fef6aa53ad57dcc318c875ee91398cf9e7f58
thakurdinker <dinker.thakur55@gmail.com>
2024-07-06 13:48:19 +0400

    added properties in the community page

--- routes/communityPage.js
+++ routes/communityPage.js
@@ -2,6 +2,7 @@ const express = require("express");
 const catchAsync = require("../utils/seedDB/catchAsync");
 const Community = require("../models/community");
 const shuffle = require("../utils/shuffleArray");
+const Property = require("../models/properties");
 
 const router = express.Router({ mergeParams: true });
 
@@ -19,12 +20,18 @@ router.route("/:communitySlug").get(
           .json({ success: false, message: "NO community Found" });
       }
 
+      // Properties in the community
+      const properties = await Property.find({
+        community_name_slug: communitySlug,
+      });
+
       const moreCommunities = shuffle(
         await Community.find({ _id: { $ne: community._id } }).limit(5)
       );
       return res.status(200).json({
         success: true,
         community,
+        properties,
         moreCommunities,
         message: "DONE",
       });

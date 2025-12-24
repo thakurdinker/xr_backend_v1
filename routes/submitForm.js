f5cc085c3d3ab06d0a69432596d629397df40c79
thakurdinker <dinker.thakur55@gmail.com>
2025-12-24 16:03:03 +0400

    added rate limiting for form submit apis

--- routes/submitForm.js
+++ routes/submitForm.js
@@ -8,13 +8,21 @@ const {
   newsletterSubmission,
 } = require("../controller/submitFormController");
 
+const { rateLimit } = require("express-rate-limit");
+
+const limiter = rateLimit({
+  windowMs: 10 * 60 * 1000, // 10 minutes
+  max: 5, // Limit each IP to 5 requests per `windowMs`
+  message: "Too many requests, please try again in 10 minutes.",
+});
+
 const router = express.Router();
 
-router.post("/contact", submitContactForm);
-router.post("/landing-page", submitLandingPageForm);
-router.post("/career_contact", submitContactFormCareer);
+router.post("/contact", limiter, submitContactForm);
+router.post("/landing-page", limiter, submitLandingPageForm);
+router.post("/career_contact", limiter, submitContactFormCareer);
 router.post("/brochure-download", brochureDownloadSubmission);
 router.post("/market-report", marketReportSubmission);
-router.post("/newsletter", newsletterSubmission);
+router.post("/newsletter", limiter, newsletterSubmission);
 
 module.exports = router;

d6338e924ee0b8699c5a51b2718848127750d2c0
thakurdinker <dinker.thakur55@gmail.com>
2026-02-01 15:13:08 +0400

    community data for all communities page now includes strapi data

--- controller/submitFormController.js
+++ controller/submitFormController.js
@@ -110,7 +110,7 @@ module.exports.submitLandingPageForm = catchAsync(async (req, res) => {
 
   console.log(req.body);
 
-  // return res.status(404).json({ error: "An error occurred while saving the form" });
+  return res.status(200).json({ error: "An error occurred while saving the form" });
   try {
     submitDataToCrm(req.body);
     res.status(201).json({ message: "Form submitted successfully" });
